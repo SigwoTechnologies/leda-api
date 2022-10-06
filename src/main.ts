e06103a4164ff1d04eced30931c045ecdc17d038
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-10-05 21:22:13 -0700

    chore: clean up useless files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const version = 'v1';
+
+  app.enableCors();
+  app.setGlobalPrefix(version);
+
+  const port = process.env.PORT || 3333;
+
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + version);
+  });
 }
 bootstrap();
