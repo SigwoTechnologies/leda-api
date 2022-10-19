bc7e7eae29505b44773df85f263c2b4551cba27e
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-10-19 12:24:51 -0700

    refactor: changes from code review

--- src/common/utils/image-utils.ts
+++ src/common/utils/image-utils.ts
@@ -4,12 +4,9 @@ const ALLOWED_FILE_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif']
 const ALLOWED_FILE_SIZE_IN_MB = 1000;
 
 export const isValidExtension = (image: Express.Multer.File) => {
-  if (!ALLOWED_FILE_TYPES.includes(image.mimetype)) return false;
-
-  return true;
+  return ALLOWED_FILE_TYPES.includes(image.mimetype);
 };
 
 export const isValidSize = (size: number) => {
-  if (size / (1024 * 1024) > ALLOWED_FILE_SIZE_IN_MB) return false;
-  return true;
+  return size / (1024 * 1024) <= ALLOWED_FILE_SIZE_IN_MB;
 };
