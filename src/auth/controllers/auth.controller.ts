a0c595a50e10fd588952ca4171ff65688d75de7f
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-10-25 19:50:19 -0700

    feat: making authenticate function protected by jwt and valid address

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,19 +4,27 @@ import { CredentialsRequestDto } from '../dto/credentials-request.dto';
 import { CredentialsResponseDto } from '../dto/credentials-response.dto';
 import { Public } from 'src/auth/decorators/public.decorator';
 import { SigninRequestDto } from '../dto/signin-request.dto';
+import { IsAddressValid } from '../decorators/address.decorator';
 
-@Public()
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('/signin')
   signin(@Body() credentials: SigninRequestDto): Promise<CredentialsResponseDto> {
     return this.authService.signin(credentials);
   }
 
+  @Public()
   @Post('/nonce')
   getNonce(@Body() authCredentialsDto: CredentialsRequestDto): Promise<string> {
     return this.authService.getNonce(authCredentialsDto);
+  }
+
+  @IsAddressValid()
+  @Post('/authenticate')
+  authenticate(@Body() authCredentialsDto: CredentialsRequestDto): string {
+    return authCredentialsDto.address;
   }
 }
