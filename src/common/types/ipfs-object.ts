a617761d524ff3c75829f14f393ffb40476431de
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-11-16 18:04:46 -0700

    refactor: clean up useless object

--- src/common/types/ipfs-object.ts
+++
@@ -1,9 +0,0 @@
-import { IpfsAttribute } from './ipfs-attribute';
-
-export type IpfsObject = {
-  description: string;
-  external_url: string;
-  image: string;
-  name: string;
-  attributes: IpfsAttribute[];
-};
