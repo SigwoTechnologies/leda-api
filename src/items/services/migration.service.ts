2b22576ed0e3910b20ff250002652458849a4445
AlefrankM <alefrank.martinez@stackitgroup.com>
2022-12-13 21:09:01 -0400

    Lola

--- src/items/services/migration.service.ts
+++ src/items/services/migration.service.ts
@@ -5,8 +5,10 @@ import { HistoryRepository } from '../repositories/history.repository';
 import { ItemLikeRepository } from '../repositories/item-like.repository';
 import { ItemRepository } from '../repositories/item.repository';
 import { VoucherRepository } from '../repositories/voucher.repository';
-import { createReadStream, readFile } from 'fs';
+import * as fs from 'fs';
 import { join } from 'path';
+import * as jsonData from '../../jup-apes-migration/jups.json';
+import { fileURLToPath } from 'url';
 
 @Injectable()
 export class MigrationService {
@@ -23,22 +25,16 @@ export class MigrationService {
     const { JUP_APES_DIRECTORY, JUP_APES_TO_UPLOAD } = process.env;
 
     // get images from an interval
-    for (let i = 0; i < Number(JUP_APES_TO_UPLOAD); i++) {
-      const imageIdx = i + 1;
-      // image path
-      const image = `${join(process.cwd())}/${JUP_APES_DIRECTORY}/images/${imageIdx}.jpeg`;
-    }
+    // for (let i = 0; i < Number(JUP_APES_TO_UPLOAD); i++) {
+    //   const imageIdx = i + 1;
+    //   // image path
+    // }
+    const file = fs.readFileSync(join(process.cwd(), './images/1188254.jpg'));
 
-    // way to read the json data
-    readFile(`${join(process.cwd())}/${JUP_APES_DIRECTORY}/jups.json`, 'utf-8', (err, data) => {
-      if (err) throw err;
-      console.log(data); // as string
-      console.log(JSON.parse(data)); // as json
-    });
+    console.log(file);
+    // console.log(jsonData);
 
-    // way to return the json
-    const file = createReadStream(`${join(process.cwd())}/${JUP_APES_DIRECTORY}/jups.json`);
-    return new StreamableFile(file);
+    return jsonData;
 
     // TODO:
     // Get bonuses file
